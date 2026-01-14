a9adeaf5979bd9c6958c94b1c71357af7e9c1a85
Arthur <you@example.com>
2026-01-14 19:19:44 -0300

    first commit

---
+++ src/main/java/com/example/LoginNext/LoginNextApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoginNext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginNextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LoginNext/LoginNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoginNext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginNextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
